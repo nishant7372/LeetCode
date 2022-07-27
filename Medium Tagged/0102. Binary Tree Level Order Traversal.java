17e59e2115154664dae237812db245a466d3143a
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-07-27 20:46:58 +0530

    Update 0102. Binary Tree Level Order Traversal.java

--- Medium Tagged/0102. Binary Tree Level Order Traversal.java
+++ Medium Tagged/0102. Binary Tree Level Order Traversal.java
@@ -1,31 +1,51 @@
+![image](https://assets.leetcode.com/users/images/70c167f6-4af7-4e6a-aede-a44490a0cb54_1658934718.0628078.png)
+
+```java 
+//Depth First Search
 class Solution {
+    private List<List<Integer>> a;
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> a = new ArrayList<List<Integer>>();
-       
-        for(int i=0;i<=maxDepth(root);i++)
-        a.add(new ArrayList<Integer>());
-        
-        levelOrder(root,0,a);
+        a = new ArrayList<List<Integer>>();
+        traverse(root,0);
         return a;
     }
     
-    private void levelOrder(TreeNode root,int c, List<List<Integer>> a)
+    private void traverse(TreeNode root,int c)
     {
         if(root==null)
             return;
+        if(a.size()<=c)
+            a.add(new ArrayList<Integer>());
         a.get(c).add(root.val);
-        levelOrder(root.left,c+1,a);
-        levelOrder(root.right,c+1,a);
+        traverse(root.left,c+1);
+        traverse(root.right,c+1);
     }
-    
-    private int maxDepth(TreeNode root)
-    {
-        if(root==null)
-            return -1;
-        
-        int lDepth = maxDepth(root.left); 
-        int rDepth = maxDepth(root.right); 
-        
-        return Math.max(lDepth+1,rDepth+1);
+}
+
+
+//Breadth First Search
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q= new LinkedList<>();
+        List<List<Integer>> a= new ArrayList<List<Integer>>();
+        if(root!=null)
+        q.add(root);
+        int j=0;
+        while(!q.isEmpty())
+        {
+            int c=q.size(); 
+            a.add(new ArrayList<Integer>());
+            for(int i=0;i<c;i++)
+            {
+                TreeNode presentNode = q.remove();
+                a.get(j).add(presentNode.val);
+                if(presentNode.left!=null)
+                    q.add(presentNode.left);
+                if(presentNode.right!=null)
+                    q.add(presentNode.right);
+            }
+            j++;
+        }
+        return a;
     }
 }
