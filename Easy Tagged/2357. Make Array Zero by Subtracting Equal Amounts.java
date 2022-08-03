3bff9dc8446f2f4efcf2327884497ca4c829dc39
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-03 10:22:06 +0530

    Update 2357. Make Array Zero by Subtracting Equal Amounts.java

--- Easy Tagged/2357. Make Array Zero by Subtracting Equal Amounts.java
+++ Easy Tagged/2357. Make Array Zero by Subtracting Equal Amounts.java
@@ -1,13 +1,27 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         //the answer will be equal to unique non-zero integers in nums
-        int[] a = new int[101];
+        int[] freq = new int[101];
         for(int x:nums)
-            a[x]=1;
-        int c=0;
-        for(int x:a)
-            if(x==1)
-                c++;
-        return a[0]==1? c-1:c;
+            freq[x]=1;
+        int count=0;     //unique non-zero integer count
+        for(int i=1;i<101;i++)
+            if(freq[i]==1)
+                count++;
+        return count;
     }
+}
+
+// using one loop only -
+class Solution {
+    public int minimumOperations(int[] nums) {
+        //the answer will be equal to unique non-zero integers in nums
+        int[] freq = new int[101];
+        int count=0;          //unique non-zero integer count
+		freq[0]=1;
+        for(int x:nums)
+            if(freq[x]++==0)
+                count++;
+        return count;
+	}
 }
