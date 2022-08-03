7e94b900148c1ff62e029a0bbcc784fc62ba76db
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-03 10:23:31 +0530

    Update 2357. Make Array Zero by Subtracting Equal Amounts.java
    1ms runtime 
    faster than 99 percent submissions

--- Easy Tagged/2357. Make Array Zero by Subtracting Equal Amounts.java
+++ Easy Tagged/2357. Make Array Zero by Subtracting Equal Amounts.java
@@ -4,7 +4,7 @@ class Solution {
         int[] freq = new int[101];
         for(int x:nums)
             freq[x]=1;
-        int count=0;     //unique non-zero integer count
+        int count=0;     //unique non-zero integers count
         for(int i=1;i<101;i++)
             if(freq[i]==1)
                 count++;
@@ -17,7 +17,7 @@ class Solution {
     public int minimumOperations(int[] nums) {
         //the answer will be equal to unique non-zero integers in nums
         int[] freq = new int[101];
-        int count=0;          //unique non-zero integer count
+        int count=0;          //unique non-zero integers count
 		freq[0]=1;
         for(int x:nums)
             if(freq[x]++==0)
