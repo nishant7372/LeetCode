f0900c929a82cccda6dfa273ecf839587d65c9f3
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-06 04:22:27 +0530

    Update 0046. Permutations.java

--- Medium Tagged/0046. Permutations.java
+++ Medium Tagged/0046. Permutations.java
@@ -1,4 +1,4 @@
-class Solution {
+ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> a = new ArrayList<List<Integer>>();
         combine(nums,a,new ArrayList<Integer>());
@@ -21,5 +21,37 @@ class Solution {
             list.remove(list.size()-1);
             }
         }
+    }
+}
+
+
+//Another Way
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        int[] freq = new int[21];
+        for(int x:nums)
+            freq[x+10]++;
+        List<List<Integer>> a = new ArrayList<List<Integer>>();
+        combine(nums,a,new ArrayList<Integer>(),freq,new int[21]);
+        return a;
+    }
+
+    private void combine(int[] nums,List<List<Integer>> a,ArrayList<Integer> list,int[] freq,int[] freq1)
+    {
+        if(list.size()==nums.length)   
+        {
+            a.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int x:nums)
+        {
+            if(freq1[x+10]<freq[x+10])
+            {
+            freq1[x+10]++;
+            list.add(x);
+            combine(nums,a,list,freq,freq1);
+            freq1[list.remove(list.size()-1)+10]--;
+            }
+        } 
     }
 }
