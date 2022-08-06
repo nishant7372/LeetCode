7bfef54a70b29cbfcd4bee0f7771906cfb817506
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-06 12:08:22 +0530

    Update 0458. Poor Pigs.java

--- Hard Tagged/0458. Poor Pigs.java
+++ Hard Tagged/0458. Poor Pigs.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int poorPigs(int buckets, int m, int n) {
-        return (int)Math.ceil(Math.log10(buckets)/Math.log10((n/m)+1));
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return (int)Math.ceil(Math.log(buckets)/Math.log((minutesToTest/minutesToDie )+1));
     }
 }
+
+//Solve for min x such that (t+1)^x>=N
+//t = minutesToTest/minutesToDie 
+//N = no. of buckets
+//x is the min no. of pigs
