10f9287eee82018d1d49cccc6a33b16c647c10a2
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-07 11:43:47 +0530

    Create 0438. Find All Anagrams in a String.java

---
+++ Medium Tagged/0438. Find All Anagrams in a String.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        if(s.length()<p.length())
+            return a;
+        String temp = s.substring(0,p.length());
+        
+        //Frequency HashTable for String p
+        int[] freq1 = new int[26];
+        //Frequency HashTable for String s
+        int[] freq2 = new int[26];
+        
+        for(int i=0;i<p.length();i++)
+        {
+            freq1[p.charAt(i)-'a']++;      //for string p
+            freq2[temp.charAt(i)-'a']++;   //for 1st substring of s
+        }
+        
+        if(equal(freq1,freq2))
+                a.add(0);
+        
+        for(int i=1;i<s.length()-p.length()+1;i++)
+        {
+            freq2[s.charAt(i-1)-'a']--;
+            freq2[s.charAt(i+p.length()-1)-'a']++;
+            if(equal(freq1,freq2))
+                a.add(i);
+        }
+        return a;
+    }
+
+    private boolean equal(int[] a,int[] b)
+    {
+        for(int i=0;i<26;i++)
+            if(a[i]!=b[i])
+                return false;
+        return true;
+    }
+}
