85f77590445030808c484b4c16f19abe38b56078
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-14 15:14:08 +0530

    Update 0049. Group Anagrams.java

--- Medium Tagged/0049. Group Anagrams.java
+++ Medium Tagged/0049. Group Anagrams.java
@@ -1,3 +1,24 @@
+// Using HashMap --->  14ms runtime
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,ArrayList<String>> map = new HashMap<>();
+        
+        for(String s:strs)
+        {
+            char[] freq = new char[26];
+            for(char ch:s.toCharArray())
+                freq[ch-'a']++;
+            String key = String.valueOf(freq);
+            if(!map.containsKey(key))
+                map.put(key,new ArrayList<String>());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
+
+// Brute Force --> ~1000ms runtime
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<List<String>>();
