4ba722ced14ad26cf9f94a1641a7babd57c7f8dd
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-28 10:17:37 +0530

    Create 0451. Sort Characters By Frequency.java

---
+++ Medium Tagged/0451. Sort Characters By Frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String frequencySort(String s) {
+        int[] a = new int[75];
+        
+        for(int i=0;i<s.length();i++)
+            a[s.charAt(i)-48]++;
+        
+        StringBuffer sb = new StringBuffer();
+        while(sb.length()!=s.length())
+        {
+        int max_freq=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]>a[max_freq])
+            {
+                max_freq=i;
+            }
+        }
+        sb.append(String.valueOf((char)(max_freq+48)).repeat(a[max_freq]));
+        a[max_freq]=0; 
+        }
+        return sb.toString();
+    }
+}
