1e5f6a72b7dd7b39579d6456a0b714d6b4fa967b
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-08-31 12:52:44 +0530

    Update 0048. Rotate Image.java

--- Medium Tagged/0048. Rotate Image.java
+++ Medium Tagged/0048. Rotate Image.java
@@ -1,26 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
-       int l1 = matrix.length;
-       for(int i=0;i<l1;i++)
+       int n = matrix.length;
+        
+       //Transpose the Matrix
+       for(int i=0;i<n;i++)
        {
-           for(int j=i;j<l1;j++)
+           for(int j=i;j<n;j++)
            {
               int temp =  matrix[i][j];
               matrix[i][j] = matrix[j][i]; 
               matrix[j][i]=temp;
            }
        }
-       for(int i=0;i<l1;i++)
+        
+       //Reverse rows of matrix 
+       for(int[] row:matrix)
        {
-           int j=0;
-           int k=l1-1;
-           while(j<k)
+           int i=0;
+           int j=n-1;
+           while(i<j)
            {
-              int temp =  matrix[i][j];
-              matrix[i][j] = matrix[i][k]; 
-              matrix[i][k]=temp;
-              j++;
-              k--;
+              int temp =  row[i];
+              row[i++] = row[j]; 
+              row[j--] = temp;
            }
        }
     }
