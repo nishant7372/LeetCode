7ce8bb8df37ba87b5996588f73f1eba261d4e6d0
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-09-25 23:18:36 +0530

    Update 0142. Linked List Cycle II.java

--- Medium Tagged/0142. Linked List Cycle II.java
+++ Medium Tagged/0142. Linked List Cycle II.java
@@ -1,3 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+//Floyd Cycle Detection Algorithm -> Optimized Solution
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+                break;
+        }
+        if(fast==null || fast.next==null)
+            return null;
+        while(slow!=head)
+        {
+            slow = slow.next;
+            head = head.next;
+        }
+        return head;
+    }
+}
+
+
+//Brute Force
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         List<ListNode> list = new ArrayList<>();
