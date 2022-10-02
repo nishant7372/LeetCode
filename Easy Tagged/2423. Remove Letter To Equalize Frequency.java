f8f1d7ef1c17288b55839c8b0b682e45231e1856
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-10-02 21:27:22 +0530

    Create 2423. Remove Letter To Equalize Frequency.java

---
+++ Easy Tagged/2423. Remove Letter To Equalize Frequency.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean equalFrequency(String word) {
+        int[] a = new int[26];
+        
+        for(int i=0;i<word.length();i++)
+            a[word.charAt(i)-'a']++;
+        
+        int max=0;
+        int min=word.charAt(0)-'a';
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]>a[max])
+                max=i;
+            if(a[i]<a[min] && a[i]>0)
+                min=i;
+        }
+        //checking by reducing max freq by 1
+        a[max]--;
+        if(check(a))
+             return true;
+        a[max]++;
+        
+        //checking by reducing min freq by 1
+        a[min]--;
+        if(check(a))
+             return true;
+        return false;
+    }
+    
+    private boolean check(int[] a)
+    {
+        int last=0;
+        for(int x:a)
+        {
+            if(x>0){
+            if(last==0)
+                last = x;
+            else if(last!=x)
+                return false;
+            }
+        }
+        return true;
+    }
+}
