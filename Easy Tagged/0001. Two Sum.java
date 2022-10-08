205ec0f6e2b91f12cd159f0eb879c65ec8f82817
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-10-08 21:04:18 +0530

    Update 0001. Two Sum.java

--- Easy Tagged/0001. Two Sum.java
+++ Easy Tagged/0001. Two Sum.java
@@ -1,3 +1,34 @@
+//Method 1 -> Optimized using HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       Map<Integer,int[]> map = new HashMap<>();
+        
+       for(int i=0;i<nums.length;i++)
+       {
+           if(map.get(nums[i])==null){
+               int[] temp = new int[]{i,-1};
+               map.put(nums[i],temp);
+           }
+           else
+               map.get(nums[i])[1]=i;
+       }
+        
+        for(int x:map.keySet())
+        {
+            if(x==target-x)
+            {
+                return new int[]{map.get(x)[0],map.get(x)[1]};
+            }
+            else if(map.get(target-x)!=null)
+            {
+                return new int[]{map.get(x)[0],map.get(target-x)[0]};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+//Method 2 -> Brute Force
 class Solution {
     public int[] twoSum(int[] nums, int target) {
       int i=0;
