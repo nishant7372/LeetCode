0446f420eb7ec3cd0b3ac97af86630eacbf9f367
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-10-08 20:59:56 +0530

    Create 0797. All Paths From Source to Target.java

---
+++ Medium Tagged/0797. All Paths From Source to Target.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(0);
+        solve(list,temp,graph,0);
+        return list;
+    }
+    
+    private void solve(List<List<Integer>> list,ArrayList<Integer> temp,int[][] graph,int i)
+    {
+        if(i==graph.length-1)
+        {
+            list.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for(int x:graph[i])
+        {
+            temp.add(x);
+            solve(list,temp,graph,x);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
