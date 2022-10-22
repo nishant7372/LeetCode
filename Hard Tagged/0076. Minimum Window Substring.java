ed0bb7cf4913fc8e878893b057a83dde3dba61dc
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-10-22 12:50:53 +0530

    Update 0076. Minimum Window Substring.java

--- Hard Tagged/0076. Minimum Window Substring.java
+++ Hard Tagged/0076. Minimum Window Substring.java
@@ -1,72 +1,63 @@
 class Solution {
     public String minWindow(String s, String t) {
         
-        int[] a = new int[52];   //frequency array for String t
-
-        for(int i=0;i<t.length();i++)
+        int[] a = new int[52]; //for String t  
+        int[] b = new int[52]; //for window of String s
+        
+        int[] res = new int[2];  //store start & end of minString
+        
+        for(int i=0;i<t.length();i++)  //for String t
         {
-            if(t.charAt(i)<=90)
+            if(t.charAt(i)<='Z')
             a[t.charAt(i)-'A']++;
             else
             a[t.charAt(i)-'a'+26]++;
         }
         
-        int left=0;
-        int right=0;
+        int left=0;  //sliding window left pointer
+        int right=0; //sliding window right pointer
         
-        int[] b= new int[52];  //frequency array for substrings of s
-     
-        if(s.charAt(0)<=90)
-            b[s.charAt(0)-'A']++;
-        else
-            b[s.charAt(0)-'a'+26]++;
-        
-        String min="";
-        boolean flag=false;
-        
-        while(flag==false&&left<s.length())
+        while(right<=s.length()&&left<s.length())
         {            
-            if(compare(a,b))
+            if(compare(a,b)) //string found
             {
-                if(min.length()==0)
-                min=s.substring(left,right+1);
-                else
+                if(res[1]-res[0]==0)
                 {
-                    if(min.length()>right-left+1)
-                        min=s.substring(left,right+1);
+                    res[0]=left;
+                    res[1]=right;
                 }
-                if(s.charAt(left)<=90)
-                    b[s.charAt(left)-'A']--;
+                else if(res[1]-res[0]>right-left)
+                {
+                    res[0]=left;
+                    res[1]=right;
+                }
+                if(s.charAt(left)<='Z')
+                    b[s.charAt(left++)-'A']--;
                 else
-                    b[s.charAt(left)-'a'+26]--;
-                left++;
+                    b[s.charAt(left++)-'a'+26]--;
             }
             else
             {
-                right++;
                 if(right<s.length())
                 {
-                    if(s.charAt(right)<=90)
-                    b[s.charAt(right)-'A']++;
+                    if(s.charAt(right)<='Z')
+                        b[s.charAt(right++)-'A']++;
                     else
-                    b[s.charAt(right)-'a'+26]++;
+                        b[s.charAt(right++)-'a'+26]++;
                 }
                 else
-                flag=true;
+                    right++;
             }
         }
-        return min;
+        return s.substring(res[0],res[1]);
     }
     
     private boolean compare(int[] a,int[] b)
     {
         for(int i=0;i<52;i++)
         {
-            if(a[i]>0)
-            {
-                 if(a[i]>b[i])
-                 return false;
-            }
+            if(a[i]>0 && a[i]>b[i])
+                return false;
         }
         return true;
     }
