d8e018e0b0edef00cdd68e1973bf3bf978e2a001
Nishant Singh <91368799+NishantSingh02@users.noreply.github.com>
2022-11-01 19:29:10 +0530

    Update 0572. Subtree of Another Tree.java

--- Easy Tagged/0572. Subtree of Another Tree.java
+++ Easy Tagged/0572. Subtree of Another Tree.java
@@ -1,3 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//Method 1
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null)
+            return false;
+        if(root.val==subRoot.val && isIdentical(root,subRoot))
+            return true;
+        if(isSubtree(root.left,subRoot))
+            return true;
+        return isSubtree(root.right,subRoot);
+    }
+    
+    private boolean isIdentical(TreeNode root,TreeNode subRoot)
+    {
+        if(root==null && subRoot==null)
+            return true;
+        if(root==null || subRoot==null)
+            return false;
+        if(root.val!=subRoot.val)
+            return false;
+        if(!isIdentical(root.left,subRoot.left))
+            return false;
+        return isIdentical(root.right,subRoot.right);
+    }
+}
+
+
+
+
+//Method 2 -> Comparing PreOrder and InOrder Traversal
+
 class Solution {
     boolean res=false;
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
