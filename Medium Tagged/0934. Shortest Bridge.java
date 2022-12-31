cb76691e30d4f0158a625de22af8419c1f794c0e
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2022-12-31 22:44:12 +0530

    Create 0934. Shortest Bridge.java

---
+++ Medium Tagged/0934. Shortest Bridge.java
@@ -0,0 +1,81 @@
+class Solution {
+    boolean[][] visited;
+    int n;
+    public int shortestBridge(int[][] grid) {
+        n=grid.length;
+        visited=new boolean[n][n];
+
+        boolean flag=false;
+        Queue<int[]> queue = new ArrayDeque<>(); //for adding boundaries of 1st island
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    dfs(i,j,grid,queue);  //traverse the 1st island found
+                    flag=true;
+                    break;
+                }  
+            }
+            if(flag)
+               break;
+        }
+        
+        return bfs(grid,queue);  //return smallest number of flipped 0's
+    }
+    
+    private int bfs(int[][] grid,Queue<int[]> queue)
+    {
+        int level=0;
+        while(!queue.isEmpty())
+        {
+            int len = queue.size();
+            
+            while(len-->0)
+            {
+                int[] curr = queue.remove();
+                int i=curr[0], j=curr[1];
+                if(grid[i][j]==1)
+                   return level;
+
+                if(i+1<n && !visited[i+1][j]){
+                   queue.add(new int[]{i+1,j});
+                   visited[i+1][j]=true;
+                }
+
+                if(i-1>=0 && !visited[i-1][j]){
+                   queue.add(new int[]{i-1,j});
+                   visited[i-1][j]=true;
+                }
+                
+                if(j+1<n && !visited[i][j+1]){
+                   queue.add(new int[]{i,j+1});
+                   visited[i][j+1]=true;
+                }
+                
+                if(j-1>=0 && !visited[i][j-1]){
+                   queue.add(new int[]{i,j-1});
+                   visited[i][j-1]=true;
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+
+
+    private void dfs(int i,int j,int[][] grid,Queue<int[]> queue)
+    {
+        if(i==-1||i==n||j==-1||j==n||visited[i][j])
+            return;
+        visited[i][j]=true;
+        if(grid[i][j]==0)
+        {
+            queue.add(new int[]{i,j});
+            return;
+        }
+        dfs(i+1,j,grid,queue);
+        dfs(i,j+1,grid,queue);
+        dfs(i-1,j,grid,queue);
+        dfs(i,j-1,grid,queue);
+    }
+}
