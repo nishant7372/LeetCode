598e94409e11fd1c0459e7df648d7d31ced4d54f
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2022-12-31 22:41:20 +0530

    Update 0547. Number of Provinces.java

--- Medium Tagged/0547. Number of Provinces.java
+++ Medium Tagged/0547. Number of Provinces.java
@@ -1,3 +1,37 @@
+//Method 1
+//Optimized --> 1ms runtime Faster than 99.93%
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int c=0; //provinces count
+        boolean[] visited = new boolean[isConnected.length];
+        for(int i=0;i<isConnected.length;i++)
+        {
+                if(!visited[i])
+                {
+                    dfs(isConnected,i,visited);
+                    c++; 
+                }
+        }
+        return c;  
+    }
+    
+    private void dfs(int[][] a,int i,boolean[] visited)
+    {
+        visited[i]=true;
+        for(int x=0;x<a.length;x++)
+        {
+            if(!visited[x] && a[i][x]==1){
+                dfs(a,x,visited);
+            }
+        }
+    }
+}
+
+
+
+//Method 2
+// 12ms runtime Faster than 12.4%
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int c=0; //provinces count
