6dc272773ac67da1a66663b9923b5da94fea6e9c
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-01 21:45:59 +0530

    Update 0290. Word Pattern.java

--- Easy Tagged/0290. Word Pattern.java
+++ Easy Tagged/0290. Word Pattern.java
@@ -1,3 +1,39 @@
+//Solution-1 
+// Spliting String Using Regex
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] a = s.split(" ");
+        if(a.length!=pattern.length())
+            return false;
+        String[] table = new String[26];
+        for(int i=0;i<a.length;i++)
+        {
+            if(table[pattern.charAt(i)-'a']==null)
+            {
+                if(isPresent(a[i],table))
+                    return false;
+                table[pattern.charAt(i)-'a']=a[i];
+            }   
+            if(!table[pattern.charAt(i)-'a'].equals(a[i]))
+                return false;
+        }
+        return true;
+    }
+    
+    private boolean isPresent(String target,String[] table)
+    {
+        for(int i=0;i<26;i++)
+        {
+            if(table[i]!=null&&table[i].equals(target))
+                return true;
+        }
+        return false;
+    }
+}
+
+
+// Solution-2 
+// Spliting String Using For Loop
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         int k=0;
