03c6ff6842ece1655a67ba520dd8fd1d65d72d4d
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-08 05:00:41 +0530

    Update 0134. Gas Station.java

--- Medium Tagged/0134. Gas Station.java
+++ Medium Tagged/0134. Gas Station.java
@@ -1,3 +1,42 @@
+//1ms runtime --> Beats 100%
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        for(int startPoint=0;startPoint<gas.length;startPoint++)
+        {
+            if(gas[startPoint]>=cost[startPoint]){
+            int endPoint = findEndPoint(startPoint,gas,cost);
+                if(endPoint==startPoint)
+                    return startPoint;
+                if(startPoint>endPoint)
+                    return -1;
+                startPoint=endPoint;
+            }
+        }
+        return -1;
+    }
+
+    // returns end point
+    private int findEndPoint(int start,int[] gas,int[] cost)
+    {
+        int end=(start==gas.length-1)? 0:start+1;
+        int remainingGas=gas[start]-cost[start];
+        while(end!=start)
+        {
+            remainingGas+=gas[end]-cost[end];
+            if(remainingGas<0)
+                return end;
+            end++;
+            if(end==gas.length)
+                end=0;
+        }
+        return start;
+    } 
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//Same code with short variables
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         for(int i=0;i<gas.length;i++)
