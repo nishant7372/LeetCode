7bc4835e68e400605830c583ae843f47febe7a18
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-28 01:02:43 +0530

    Update 0215. Kth Largest Element in an Array.java

--- Medium Tagged/0215. Kth Largest Element in an Array.java
+++ Medium Tagged/0215. Kth Largest Element in an Array.java
@@ -1,6 +1,47 @@
+// Method 1 --> Counting Sort --> O(n+k) solution
+// Time --> O(n+k)
+// Space --> O(n+k)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        for(int x:nums){
+            if(x>max)
+               max=x;
+            if(x<min)
+               min=x;
+        }
+
+        int[] a = new int[max-min+1];
+
+        for(int x:nums){
+            a[x-min]++;
+        }
+
+        for(int i=a.length-1;i>=0;i--){
+            while(a[i]-->0){
+                k--;
+                if(k==0)
+                   return i+min;
+            }
+        }
+        return 0;
+    }
+}
+
+// Method 2 --> Sorting --> O(nlogn) solution
+// Time --> O(nlogn)
+// Space --> O(1)
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length-k];
     }
 }
+
+// Method 3 --> QuickSelect Solution 
+
+// to be updated soon
