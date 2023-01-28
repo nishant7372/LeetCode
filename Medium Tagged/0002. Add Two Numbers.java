a6bcb5672ea5c3fc687284250dc43bbcf6ff4135
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-28 15:04:24 +0530

    Update 0002. Add Two Numbers.java

--- Medium Tagged/0002. Add Two Numbers.java
+++ Medium Tagged/0002. Add Two Numbers.java
@@ -1,13 +1,38 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Approach 1 -> 2ms runtime Beats 96%
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry=0;
+        //A sentinel node is a dummy node that goes at the front of a list
+        ListNode root = new ListNode(0); //sentinel node
+        ListNode head = root;
+        while(l1!=null || l2!=null){
+            int sum=0;
+            if(l1!=null && l2!=null)
+               sum=l1.val+l2.val+carry;
+            else if(l1!=null)
+               sum=l1.val+carry;
+            else if(l2!=null)
+               sum=l2.val+carry;
+            
+            head.next = new ListNode(sum%10);
+            head=head.next;
+            carry = sum/10;
+            if(l1!=null)
+               l1=l1.next;
+            if(l2!=null)
+               l2=l2.next;
+        }
+        if(carry>0)
+           head.next = new ListNode(carry);
+        return root.next;
+    }
+
+}
+
+
+// Approach 2 --> 3ms runtime Beats 45%
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry=0;
@@ -48,5 +73,15 @@ class Solution {
              prev.next = new ListNode(carry);
         return root;
     }
-
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
