fc5bca5dc9d2c7dd916c65a5910df8ec1eb8781b
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-28 15:15:21 +0530

    Update 0739. Daily Temperatures.java

--- Medium Tagged/0739. Daily Temperatures.java
+++ Medium Tagged/0739. Daily Temperatures.java
@@ -1,3 +1,23 @@
+// Approach 1 (185ms runtime Beats 45%)
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for(int i=0;i<temperatures.length;i++)
+        {
+            while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i]){
+                int idx = st.pop();
+                res[idx]=i-idx;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+}
+
+// Approach 2 (208ms runtime Beats 26.1%)
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
        
