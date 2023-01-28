f1eca607da1b703fd4f5724e0e41633b3b53bc87
Nishant Singh <91368799+nishant7372@users.noreply.github.com>
2023-01-28 15:12:20 +0530

    Update 0973. K Closest Points to Origin.java

--- Medium Tagged/0973. K Closest Points to Origin.java
+++ Medium Tagged/0973. K Closest Points to Origin.java
@@ -1,3 +1,18 @@
+// Method 1 -->  Using Sorting with Comparator 
+// 33ms runtime Beats 77%
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points,(x,y) -> Integer.compare((x[0]*x[0]+x[1]*x[1]),(y[0]*y[0]+y[1]*y[1])));
+        return Arrays.copyOf(points,k);
+    }
+}
+
+
+// Method 2 --> Using Priority Queue with Comparator
+// Approach 1 --> Explicit Custom Comparator
+// 42ms runtime Beats 55%
+
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new ArrayComparator());
@@ -21,5 +36,24 @@ class ArrayComparator implements Comparator<int[]> {
             return 1;
         }
         return 0;
+    }
+}
+
+
+// Method 2 --> Using Priority Queue with Comparator
+// Approach 2 --> Inline Comparator
+// 42ms runtime Beats 55%
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>((x,y) -> Integer.compare((x[0]*x[0]+x[1]*x[1]),(y[0]*y[0]+y[1]*y[1])));
+        
+        for(int[] x:points){
+            queue.add(x);
+        }
+        int[][] res= new int[k][];
+        for(int i=0;i<k;i++)
+            res[i]=queue.remove();
+        return res;
     }
 }
