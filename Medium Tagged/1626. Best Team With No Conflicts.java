3612a608ece19e418f872184b3101733a69f945e
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-01-31 07:41:14 +0530

    Update 1626. Best Team With No Conflicts.java

--- Medium Tagged/1626. Best Team With No Conflicts.java
+++ Medium Tagged/1626. Best Team With No Conflicts.java
@@ -1,6 +1,16 @@
 // O(n^2) time and O(n) space DP Solution with Sorting
 // Beats 83% Java
 
+
+// Approach:
+
+// 1. Sort the Players on the basis of scores(Ascending) and break their ties on the basis of age(Ascending).
+// 2. Now, we can select the players from left to right.
+// 3. Create a dp array of length n, where dp[i], indicates the sum of scores of selected players on left of ith player + score of ith player.
+// 4. For each player i, now search a player j on left which has maximum dp[j] value and age[j]<=age[i].
+// 5. Fill dp[i] with searched value(if not found then 0) + curr player score.
+// 6. Get the max from dp.
+    
 class Solution {
     public int bestTeamScore(int[] scores, int[] ages) {
         int n = scores.length;
