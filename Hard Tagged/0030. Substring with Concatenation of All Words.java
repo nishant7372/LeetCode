06ee2405da41126584a1f8f187b450bb270cf981
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-01 22:36:12 +0530

    Update 0030. Substring with Concatenation of All Words.java

--- Hard Tagged/0030. Substring with Concatenation of All Words.java
+++ Hard Tagged/0030. Substring with Concatenation of All Words.java
@@ -1,39 +1,32 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         int wordLen = words.length*words[0].length();
+
+        Map<String,Integer> map = new HashMap<>();
+        for(String w:words)
+            map.put(w,map.getOrDefault(w,0)+1);
         
+        char[] arr = s.toCharArray();
         for(int i=0;i<s.length()-wordLen+1;i++)
         {
-            if(check(s,i,i+wordLen,words))
+            if(check(arr,i,i+wordLen,new HashMap<>(map),words[0].length()))
                 list.add(i);  
         }
         return list;
     }
     
-    private boolean check(String s,int start,int end,String[] words)
-    {
-        Map<String,Integer> map = new HashMap<>();
-        for(String w:words)
-        {
-            if(map.get(w)!=null)
-               map.put(w,map.get(w)+1);
-            else
-               map.put(w,1);
-        } 
-        int gap = words[0].length();
+    private boolean check(char[] arr,int start,int end,Map<String,Integer> map,int gap)
+    {   
         for(int i=start;i<end;i+=gap)
         {
-            String t = s.substring(i,i+gap);
-            if(map.get(t)!=null)
+            String t = String.valueOf(arr,i,gap);
+            if(map.get(t)!=null && map.get(t)>0)
                map.put(t,map.get(t)-1);
             else
                return false;
-            if(map.get(t)<0)
-                return false;
         }
-        for(int x:map.values())
-        {
+        for(int x:map.values()){
             if(x!=0)
                 return false;
         }
