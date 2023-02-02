266dd5418806ad0e4a87624fc1ceecb6742f63d8
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-02 19:07:00 +0530

    Update 0014. Longest Common Prefix.java

--- Easy Tagged/0014. Longest Common Prefix.java
+++ Easy Tagged/0014. Longest Common Prefix.java
@@ -25,3 +25,50 @@ class Solution {
     }
 }
 
+// Beats 78.39% --> 1ms runtime (using Trie)
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Trie trie = new Trie();
+        for(String s:strs)
+            insert(trie,s);
+        StringBuilder sb = new StringBuilder();
+        while(trie.count==1&&!trie.stop){
+            trie = get(trie.a,sb);
+        }
+        return sb.toString();
+    }
+
+    private void insert(Trie trie,String s){
+        for(char ch:s.toCharArray()){
+            if(trie.a[ch-'a']==null){
+               trie.a[ch-'a']=new Trie();
+               trie.count++;
+            }
+            trie = trie.a[ch-'a'];
+        }
+        trie.stop=true;
+    }
+
+    private Trie get(Trie[] a,StringBuilder sb){
+        for(int i=0;i<26;i++){
+            if(a[i]!=null){
+               sb.append((char)(i+'a'));
+               return a[i];
+            }
+        }
+        return null;
+    }
+}
+
+
+class Trie{
+    Trie[] a;
+    boolean stop;
+    int count;
+    public Trie(){
+        this.a = new Trie[26];
+        this.stop = false;
+        this.count = 0;
+    }
+}
