41d9dc861c6623d7709e6a631f9f9e0c196466de
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-03 12:06:31 +0530

    Update 0008. String to Integer (atoi).java

--- Medium Tagged/0008. String to Integer (atoi).java
+++ Medium Tagged/0008. String to Integer (atoi).java
@@ -1,32 +1,25 @@
 class Solution {
     public int myAtoi(String s) {
-        s=s.trim();
-        boolean neg = false;
+        s=s.trim(); // remove all trailing wwith spaces
+        boolean neg = false; // flag to check whether no. is negative
         if(s.equals(""))
             return 0;
         if(s.charAt(0)=='-')
             neg=true;
-        StringBuffer t = new StringBuffer("0");
+        StringBuilder t = new StringBuilder("0");
         for(int i= (s.charAt(0)=='+'||s.charAt(0)=='-')? 1:0;i<s.length();i++)
         {
-            if(s.charAt(i)>='0'&&s.charAt(i)<='9')
+            if(Character.isDigit(s.charAt(i)))
                 t.append(s.charAt(i));
             else
                 break;
         }
-        int a=0;
         try{
-        a = Integer.parseInt(t.toString());
+            int a = Integer.parseInt(t.toString());
+            return neg? -a:a;
         }
-        catch(Exception e)
-        {
-            if(neg)
-                return Integer.MIN_VALUE;
-            else
-                return Integer.MAX_VALUE;
+        catch(Exception e){
+            return neg? Integer.MIN_VALUE: Integer.MAX_VALUE;
         }
-        if(neg)
-            return -1*a;
-        return a;
     }
 }
