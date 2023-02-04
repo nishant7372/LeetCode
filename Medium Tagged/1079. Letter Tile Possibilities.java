0cc24200fdf39e0a13e50b2e41c35d95d95b4ffa
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-04 10:50:57 +0530

    Update 1079. Letter Tile Possibilities.java

--- Medium Tagged/1079. Letter Tile Possibilities.java
+++ Medium Tagged/1079. Letter Tile Possibilities.java
@@ -1,24 +1,56 @@
+// Method 1 --> 40ms runtime Beats 35% (Basic Approach)
+
 class Solution {
     public int numTilePossibilities(String tiles) {
-        int[] freq = new int[26];
-        for(int i=0;i<tiles.length();i++)
-            freq[tiles.charAt(i)-'A']++;
-        Set<String> a = new HashSet<>();
-        generate(a,tiles,"",freq,new int[26]);
-        return a.size();
+        char[] arr = tiles.toCharArray();
+        boolean[] visited = new boolean[tiles.length()];
+        Set<String> set = new HashSet<>();
+        generate(set,arr,"",visited);
+        return set.size();
     }
     
-    private void generate(Set<String> a,String tiles,String temp,int[] freq,int[] freq1)
+    private void generate(Set<String> set,char[] tiles,String temp,boolean[] visited)
     {
         if(temp.length()!=0)
-        a.add(temp);
-        for(int i=0;i<tiles.length();i++)
+           set.add(temp);
+        
+        int i=0;
+        for(char ch:tiles)
         {
-            if((freq1[tiles.charAt(i)-'A'])<(freq[tiles.charAt(i)-'A']))
+            if(!visited[i]){
+                visited[i]=true;
+                generate(set,tiles,temp+ch,visited);
+                visited[i]=false;
+            }
+            i++;
+        }    
+    }
+}
+
+// Method 2 --> 115ms runtime Beats 10% (Basic Approach)
+
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        char[] arr = tiles.toCharArray();
+        int[] freq = new int[26];
+        for(char ch:arr)
+            freq[ch-'A']++;
+        Set<String> set = new HashSet<>();
+        generate(set,arr,"",freq,new int[26]);
+        return set.size();
+    }
+    
+    private void generate(Set<String> set,char[] tiles,String temp,int[] total,int[] freq)
+    {
+        if(temp.length()!=0)
+           set.add(temp);
+        for(char ch:tiles)
+        {
+            if(freq[ch-'A']<total[ch-'A'])
             {
-                freq1[tiles.charAt(i)-'A']++;
-                generate(a,tiles,temp+tiles.charAt(i),freq,freq1);
-                freq1[tiles.charAt(i)-'A']--;
+                freq[ch-'A']++;
+                generate(set,tiles,temp+ch,total,freq);
+                freq[ch-'A']--;
             }
         }    
     }
