957ac2c396e8cae5e8c4b2b980159a3c6f02c76f
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-05 23:30:49 +0530

    Update 1224. Maximum Equal Frequency.java

--- Hard Tagged/1224. Maximum Equal Frequency.java
+++ Hard Tagged/1224. Maximum Equal Frequency.java
@@ -4,35 +4,42 @@
 
 class Solution {
     public int maxEqualFreq(int[] nums) {
-        int max=0;
+        int max=0; // gives size of hashtable
         for(int x:nums)
             if(x>max)
                 max=x;
-        int[] map = new int[100001];
-        int[] a = new int[max+1];
-        max=1;
+        
+        int[] a = new int[max+1]; // mapping element -> freq
+        int[] map = new int[nums.length+1]; // mapping freq -> no. of elements
+        
+        max=1; //using max for max Result
         int maxFreq=0, minFreq=Integer.MAX_VALUE, count=0;
+        // count = no. of unique frequencies
+
         for(int i=0;i<nums.length;i++){
             int freq =  ++a[nums[i]];
             if(map[freq]++==0){ // adding curr Freq
                 count++;
             }
             if(freq>1 && map[freq-1]--==1){ // remove prev freq
-                count--;
-                if(freq-1==minFreq)
+                count--; 
+                if(freq-1==minFreq) // updating minFreq
                 minFreq++;
             }
 
-            if(freq>maxFreq)
+            if(freq>maxFreq) // updating maxFreq
                 maxFreq=freq;
-            if(freq<minFreq)
+            if(freq<minFreq)  //updating minFreq
                 minFreq=freq;
-    
+            
+            // unique frequencies == 1 && (maxFreq==1 || no. of elements at maxFreq==1)
             if(count==1 && (maxFreq==1 || map[maxFreq]==1)){
                 max=Math.max(max,i+1);
             }
             
+            // unique frequencies == 2
             if(count==2){
+                // element to be removed has (freq 1 || freq same as others+1)
                 if(map[1]==1 || (map[maxFreq]==1 && maxFreq==minFreq+1))
                    max=Math.max(max,i+1);
             }
