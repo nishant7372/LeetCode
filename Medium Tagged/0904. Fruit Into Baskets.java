74583100e1fef73049a84ced6870361178d3af08
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-05 17:33:09 +0530

    Update 0904. Fruit Into Baskets.java

--- Medium Tagged/0904. Fruit Into Baskets.java
+++ Medium Tagged/0904. Fruit Into Baskets.java
@@ -1,23 +1,26 @@
+//Beats 97% O(n)time and O(1) space No HashMap
+
 class Solution {
     public int totalFruit(int[] fruits) {
-        int a = 0;   //index of first tree
-        int b = -1;  //index of second tree
-        int max=1;  //maximum count
-        int c=1;   //count for each window
-        int lastChange=0;  //initializing the last change
+        int b1 = 0, b2 = -1; //initially b1 contains fruit at index 0
+        int max=1; //maximum no. of fruits
+        int c=1; //count
+        int lastChange=0; //lastchange index
         for(int i=1;i<fruits.length;i++)
         {
-            if(fruits[i]!=fruits[a] && b==-1)
-             b=i;
-            if(b!=-1&&(fruits[i]!=fruits[b]&&fruits[i]!=fruits[a]))
-            {
-                 max=Math.max(c,max);
-                 a=lastChange;
-                 c=i-a;
-                 b = i;
+            if(fruits[i]!=fruits[b1] && b2==-1){
+                b2 = i;
             }
-            if(fruits[i]!=fruits[i-1])
-                lastChange=i;      //remebering the last change
+            else if(fruits[i]!=fruits[b1] && fruits[i]!=fruits[b2])
+            {
+                max=Math.max(c,max);
+                b1 = lastChange;
+                b2 = i;
+                c = i-b1;
+            }
+            if(fruits[i]!=fruits[i-1]){
+                lastChange=i;
+            }
             c++;
         }
         return Math.max(max,c);
