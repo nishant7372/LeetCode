01ace8bca79226e9c92906a717bece10b42741ba
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-05 13:28:08 +0530

    Update 0242. Valid Anagram.java

--- Easy Tagged/0242. Valid Anagram.java
+++ Easy Tagged/0242. Valid Anagram.java
@@ -1,3 +1,27 @@
+// Method 1 --> Beats 100% 1ms runtime
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        int[] a = new int[26];
+        int[] b = new int[26];
+        
+        for(char ch:s.toCharArray())
+            a[ch-'a']++;
+         for(char ch:t.toCharArray())
+            b[ch-'a']++;
+        
+        for(int i=0;i<26;i++)
+            if(a[i]!=b[i])
+                return false;
+        return true;
+    }
+}
+
+
+// Method 2 --> Beats 60% 4ms runtime
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
