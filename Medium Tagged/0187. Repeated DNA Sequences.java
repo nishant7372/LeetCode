436753ac9a30ba4f9ba4d571130e79b98ebda5f3
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-05 16:13:30 +0530

    Update 0187. Repeated DNA Sequences.java

--- Medium Tagged/0187. Repeated DNA Sequences.java
+++ Medium Tagged/0187. Repeated DNA Sequences.java
@@ -1,3 +1,25 @@
+// Using HashMap
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        char[] arr = s.toCharArray();
+        Map<String,Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<s.length()-9;i++)
+        {
+            String t = String.valueOf(arr,i,10);
+            map.put(t,map.getOrDefault(t,0)+1);
+        }
+        for(var e:map.entrySet()){
+            if(e.getValue()>1)
+                list.add(e.getKey());
+        }
+        return list;
+    }
+}
+
+// Using 2 HashSets
+
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         Set<String> set = new HashSet<>();
