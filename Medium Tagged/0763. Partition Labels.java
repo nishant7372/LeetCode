fe76ee82927ba66d09e11ae4635728b576920431
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-07 18:33:25 +0530

    Update 0763. Partition Labels.java

--- Medium Tagged/0763. Partition Labels.java
+++ Medium Tagged/0763. Partition Labels.java
@@ -1,20 +1,20 @@
+// Beats 99% O(n) solution
+
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> a = new ArrayList<Integer>();
         int[] lIndex = new int[26];
         
         for(int i=0;i<26;i++)
-            lIndex[i]=s.lastIndexOf((char)(i+97));
+            lIndex[i]=s.lastIndexOf((char)(i+'a'));
         
         int index=0;
         int lastIndex=lIndex[s.charAt(index)-'a'];
         while(index<s.length())
         {
             for(int i=index;i<lastIndex;i++)
-            {
-                if(lIndex[s.charAt(i)-'a']>lastIndex)
-                    lastIndex=lIndex[s.charAt(i)-'a'];
-            }
+                lastIndex = Math.max(lastIndex,lIndex[s.charAt(i)-'a']);
+    
             a.add(lastIndex-index+1);
             index=lastIndex+1;
             
