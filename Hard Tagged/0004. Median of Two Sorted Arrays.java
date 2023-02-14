95baa2920fbf65c9a513e03e9defee760980af78
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-14 17:51:26 +0530

    Update 0004. Median of Two Sorted Arrays.java

--- Hard Tagged/0004. Median of Two Sorted Arrays.java
+++ Hard Tagged/0004. Median of Two Sorted Arrays.java
@@ -1,36 +1,21 @@
+// 0ms runtime Beats 100% 
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int n1 = nums1.length, n2 = nums2.length;
         int n = n1+n2;
         int[] a = new int[n];
          
-        int k=0;
-        int j=0;
-        int i=0;
-        while(k<n1&&j<n2)
-        {
-           if(nums1[k]<=nums2[j])
-           {
-               a[i++]=nums1[k];
-               k++;
-           }
-           else if(nums1[k]>nums2[j])
-           {
-               a[i++]=nums2[j];
-               j++;
-           }
-        }
-        while(k<n1)
-        {
-            a[i++]=nums1[k];
-            k++;
-        }
+        int i=0, j=0, k=0;
+        while(i<n1&&j<n2)
+           a[k++] = nums1[i]<=nums2[j]? nums1[i++]: nums2[j++];
+        
+        while(i<n1)
+            a[k++]=nums1[i++];
+        
         while(j<n2)
-        {
-            a[i++]=nums2[j];
-            j++;
-        }
+            a[k++]=nums2[j++];
+        
         return n%2==0? (double)(a[n/2]+a[(n/2)-1])/2:a[n/2];
     }
 }
