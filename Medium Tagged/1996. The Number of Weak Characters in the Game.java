c095c4cd11b3e7fda6268455707a1a3ddb1d2111
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-14 18:27:25 +0530

    Update 1996. The Number of Weak Characters in the Game.java

--- Medium Tagged/1996. The Number of Weak Characters in the Game.java
+++ Medium Tagged/1996. The Number of Weak Characters in the Game.java
@@ -1,33 +1,19 @@
+// Beats 46%
+
 class Solution {
-    public int numberOfWeakCharacters(int[][] properties) {
-        Arrays.sort(properties,new ArrayComparator());
+    public int numberOfWeakCharacters(int[][] p) {
+        int n = p.length;
+        // sorted by attack(increasing) and breaking ties by defense(decreasing).
+        Arrays.sort(p,(a,b)->a[0]==b[0]? Integer.compare(b[1],a[1]):Integer.compare(a[0],b[0]));
 
-        int max= properties[properties.length-1][1];
-        
-        int weakCharacters=0;
-        for(int i=properties.length-2;i>=0;i--)
+        int max= p[n-1][1];
+        int weak=0;
+        for(int i=n-2;i>=0;i--)
         {
-            if(properties[i][1]<max)
-                weakCharacters++;
-            else
-                max=properties[i][1];
+            if(p[i][1]<max)
+                weak++;
+            max=Math.max(max,p[i][1]);
         }
-        return weakCharacters;
-    }
-}
-
-class ArrayComparator implements Comparator<int[]>{
-    @Override
-    public int compare(int[] a,int[] b)
-    {
-        if(a[0]<b[0]){
-            return -1;
-        }
-        if(a[0]>b[0]){
-            return 1;
-        }
-        else{
-            return Integer.compare(b[1],a[1]);
-        }
+        return weak;
     }
 }
