f0c7eddddfcbf451b1a54ab2bcc3f965314af697
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-14 22:20:48 +0530

    Update 0005. Longest Palindromic Substring.java

--- Medium Tagged/0005. Longest Palindromic Substring.java
+++ Medium Tagged/0005. Longest Palindromic Substring.java
@@ -1,35 +1,65 @@
+// Beats 79%
+// Using Substring Updation
+
 class Solution {
     private int max=0;
     public String longestPalindrome(String s) {
-        String t="";
-        for(int i=0;i<s.length();i++)
-        {
-           t=func(i,i+1,s,t);
+        int n = s.length();
+        String res="";
+        for(int i=0;i<n;i++){
+           res=find(i,i+1,s,res);
         }
-        for(int i=1;i<s.length();i++)
-        {
-           t=func(i-1,i+1,s,t);
+        for(int i=1;i<n;i++){
+           res=find(i-1,i+1,s,res);
         }
-        if(max==0)
-        return s.charAt(0)+"";
-        return t;
+        return max==0? s.charAt(0)+"" : res;
     }
     
-    private String func(int j,int k,String s,String t)
+    private String find(int j,int k,String s,String t)
     {
         int c=0;
         while(j>=0&&k<s.length())
         {
-            if(s.charAt(j)==s.charAt(k))
-            c++;
-            else
+            if(s.charAt(j)!=s.charAt(k))
                 break;
+            c++;
             j--;
             k++;
         }
         max=Math.max(c,max);
-        if(max==c)
-        t=s.substring(j+1,k);
-        return t;
+        return max==c? s.substring(j+1,k):t;
+    }
+}
+
+
+// Beats 60% 
+// Tracking begin and end index of longest palindromic substring
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int[] res = new int[]{0,1};
+        for(int i=0;i<s.length();i++){
+            int[] temp = find(i,i+1,s);
+            if(res[1]-res[0]<temp[1]-temp[0])
+               res=temp;
+        }
+        for(int i=1;i<s.length();i++) {
+            int[] temp = find(i-1,i+1,s);
+            if(res[1]-res[0]<temp[1]-temp[0])
+               res=temp;
+        }
+        return s.substring(res[0],res[1]);
+    }
+    
+    private int[] find(int j,int k,String s)
+    {
+        while(j>=0&&k<s.length())
+        {
+            if(s.charAt(j)!=s.charAt(k))
+                return new int[]{j+1,k};
+            j--;
+            k++;
+        }
+        return new int[]{j+1,k};
     }
 }
