87b460a1107455ccae95a0848a641c0591bb56b5
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-16 15:09:21 +0530

    Update 0063. Unique Paths II.java

--- Medium Tagged/0063. Unique Paths II.java
+++ Medium Tagged/0063. Unique Paths II.java
@@ -1,35 +1,43 @@
+// 0ms runtime Beats 100% 
+// Method 1 -> Tabulation (Bottom Up Approach)
+
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp= new int[obstacleGrid.length][obstacleGrid[0].length];
-        if(obstacleGrid[0][0]==1)
-            dp[0][0]=0;
-        else
-            dp[0][0]=1;
-        for(int i=1;i<dp.length;i++)
-        {
-            if(obstacleGrid[i][0]==1)
-                    dp[i][0]=0;
-            else
-                 dp[i][0]=dp[i-1][0];   //dp state equation for 1st row
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int[][] dp= new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0]==1? 0:1;
+        for(int i=1;i<dp.length;i++){
+            dp[i][0] = grid[i][0]==1? 0:dp[i-1][0]; //dp state equation for 1st col
         }
            
-        for(int i=1;i<dp[0].length;i++)
-        {
-            if(obstacleGrid[0][i]==1)
-                    dp[0][i]=0;
-            else
-                 dp[0][i]=dp[0][i-1];   //dp state equation for 1st col
+        for(int j=1;j<dp[0].length;j++){
+            dp[0][j]= grid[0][j]==1? 0:dp[0][j-1];   //dp state equation for 1st row
         }
-        for(int i=1;i<dp.length;i++)
-        {
-            for(int j=1;j<dp[0].length;j++)
-            {
-                if(obstacleGrid[i][j]==1)
-                    dp[i][j]=0;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];  //dp state equation
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                dp[i][j] = grid[i][j]==1? 0: dp[i-1][j]+dp[i][j-1]; //dp state equation
             }
         }
         return dp[dp.length-1][dp[0].length-1];
+    }
+}
+
+// 0ms runtime Beats 100% 
+// Method 2 -> Memoization (Top Down Approach)
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        // Method 2 -> Memoization (Top Down Approach)
+        Integer[][] dp= new Integer[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0]==1? 0:1;
+        return solve(dp,grid,grid.length-1,grid[0].length-1);
+    }
+
+    private int solve(Integer[][] dp,int[][] grid,int i,int j){
+        if(i==-1 || j==-1 || grid[i][j]==1)
+           return 0;
+        if(dp[i][j]!=null)
+            return dp[i][j];
+        int res1 = solve(dp,grid,i-1,j);
+        int res2 = solve(dp,grid,i,j-1);
+        return dp[i][j] = res1+res2;
     }
 }
