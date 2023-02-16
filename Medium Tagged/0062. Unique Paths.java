4e989188bda30ccaf12d6cbe5e2f6d7a57fb7167
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-16 15:12:48 +0530

    Update 0062. Unique Paths.java

--- Medium Tagged/0062. Unique Paths.java
+++ Medium Tagged/0062. Unique Paths.java
@@ -1,3 +1,6 @@
+// 0ms runtime Beats 100%
+// Method 1 -> Tabulation --> Bottom Up Approach
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
@@ -13,5 +16,23 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
+    }
+}
+
+// 0ms runtime Beats 100%
+// Method 2 -> Memoization --> Top Down Appoach
+class Solution {
+    public int uniquePaths(int m, int n) {  
+        int[][] dp = new int[m][n];
+        return solve(dp,m-1,n-1);
+    }
+
+    private int solve(int[][] dp,int i,int j){
+        if(i==0 || j==0){
+            return 1;
+        }
+        if(dp[i][j]!=0)
+           return dp[i][j];
+        return dp[i][j]=solve(dp,i-1,j)+solve(dp,i,j-1);
     }
 }
