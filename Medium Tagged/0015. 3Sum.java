fcf3b66d657b48e271a50af9a1f3e626fd6f0192
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-02-27 11:20:22 +0530

    Update 0015. 3Sum.java

--- Medium Tagged/0015. 3Sum.java
+++ Medium Tagged/0015. 3Sum.java
@@ -1,70 +1,48 @@
+// 38ms runtime 
+// Without Set
+// Beats 72%
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-            
-            List<Integer> temp;
-            int j=1;
-            int k=nums.length-1;
-            while(j<k)
-            {
-               if(nums[j]+nums[k]>-nums[0])
-                   k--;
-               else if(nums[j]+nums[k]<-nums[0])
-                   j++;
-               else if(nums[j]+nums[k]==-nums[0])
-               {
-                   temp = new ArrayList<Integer>();
-                   temp.add(nums[0]);
-                   temp.add(nums[j]);
-                   temp.add(nums[k]);
-            
-                   if(notPresent(temp,res))
-                   res.add(temp);
-                   
-                   j++; 
-               } 
-            }
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
         
-        for(int i=1;i<nums.length-2;i++)
+        for(int i=0;i<n-2;i++)
         {
             if(nums[i]>0)
-                break;
+                return res;
             
-            if(nums[i]!=nums[i-1])
+            if(i==0 || nums[i]!=nums[i-1])
             {
-            j=i+1;
-            k=nums.length-1;
-            while(j<k)
-            {
-               if(nums[j]+nums[k]>-nums[i])
-                   k--;
-               else if(nums[j]+nums[k]<-nums[i])
-                   j++;
-               else if(nums[j]+nums[k]==-nums[i])
-               {
-                   temp = new ArrayList<Integer>();
-                   temp.add(nums[i]);
-                   temp.add(nums[j]);
-                   temp.add(nums[k]);
+                int j=i+1, k=n-1;
+                while(j<k)
+                {
+                    if(nums[j]+nums[k]>-nums[i])
+                       k--;
+                    else if(nums[j]+nums[k]<-nums[i])
+                       j++;
+                    else if(nums[j]+nums[k]==-nums[i])
+                    {
+                        List<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
                    
-                    if(notPresent(temp,res))
-                   res.add(temp);
-                   
-                   j++; 
-               } 
-            }
+                        addIfNotPresent(temp,res);
+                        j++; 
+                    } 
+                }
             }
         }
         return res;
     }
 
-    private boolean notPresent(List<Integer> a, List<List<Integer>> res)
+    private void addIfNotPresent(List<Integer> a, List<List<Integer>> res)
     {
         if(res.size()==0)
-            return true;
-        if(res.get(res.size()-1).equals(a))
-            return false;
-        return true;
+            res.add(a);
+        else if(!res.get(res.size()-1).equals(a))
+            res.add(a);
     }
 }
