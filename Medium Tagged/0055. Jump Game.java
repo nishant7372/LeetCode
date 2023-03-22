fa4daf81a3e4a563ae095d366cd5ce9a5669e73d
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-03-22 23:49:49 +0530

    Update 0055. Jump Game.java

--- Medium Tagged/0055. Jump Game.java
+++ Medium Tagged/0055. Jump Game.java
@@ -1,17 +1,40 @@
+// Greedy
 class Solution {
     public boolean canJump(int[] nums) {
         int reach=0;
         int i=0;
-        for(int x:nums)
-        {
-            if(i>reach)
+        for(int x:nums){
+            if(reach<i){
                 return false;
-            if(reach<i+x)
-                reach=i+x;
-            if(reach==nums.length-1)
+            }
+            reach = Math.max(reach,i+x);
+            if(reach>=nums.length-1){
                 return true;
+            }
             i++;
         }
-        return true;
+        return false;
+    }
+}
+
+// DP - Memoization, Top-down Approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        Boolean[] dp = new Boolean[nums.length-1];
+        return solve(nums,0,dp);
+    }
+
+    private boolean solve(int[] nums,int i,Boolean[] dp){
+        if(i>=nums.length-1){
+            return true;
+        }
+        if(dp[i]!=null){
+            return dp[i];
+        }
+        for(int j=nums[i]+i;j>i;j--){
+            if(solve(nums,j,dp))
+                return true;
+        }
+        return dp[i] = false;
     }
 }
