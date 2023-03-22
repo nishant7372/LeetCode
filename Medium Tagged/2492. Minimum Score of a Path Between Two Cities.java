4a43ab747adbe87e7fed7dc4e360a446a3cec2a2
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-03-23 00:16:36 +0530

    Update 2492. Minimum Score of a Path Between Two Cities.java

--- Medium Tagged/2492. Minimum Score of a Path Between Two Cities.java
+++ Medium Tagged/2492. Minimum Score of a Path Between Two Cities.java
@@ -1,7 +1,39 @@
+// Depth First Search
+
 class Solution {
     public int minScore(int n, int[][] roads) {
         ArrayList<int[]>[] a = new ArrayList[n+1];
+        
+        for(int i=0;i<=n;i++) {
+            a[i]=new ArrayList<int[]>();
+        }
+        for(int[] x:roads){
+            a[x[0]].add(new int[]{x[1],x[2]});
+            a[x[1]].add(new int[]{x[0],x[2]});
+        }
     
+        int[] vis = new int[n+1];
+        return dfs(a,1,vis);
+    }
+
+    private int dfs(ArrayList<int[]>[] a,int curr,int[] vis){
+        if(vis[curr]==1)
+            return Integer.MAX_VALUE;
+        vis[curr]=1;
+        int min = Integer.MAX_VALUE;
+        for(int[] x:a[curr]){    
+            min = Math.min(min,Math.min(x[1],dfs(a,x[0],vis)));
+        }
+        return min;
+    }
+}
+
+// Breadth First Search
+
+class Solution {
+    public int minScore(int n, int[][] roads) {
+        ArrayList<int[]>[] a = new ArrayList[n+1];
+        
         for(int i=0;i<=n;i++)
         {
             a[i]=new ArrayList<int[]>();
@@ -17,25 +49,18 @@ class Solution {
         queue.add(1);
         
         int min = Integer.MAX_VALUE;
-        int ans = Integer.MAX_VALUE;
         int[] vis = new int[n+1];
         while(!queue.isEmpty())
         {
             int curr = queue.remove();
-            if(curr==n)
-                ans = Math.min(min,ans);
             if(vis[curr]==1)
                continue;
             vis[curr]=1;
-            for(int[] x:a[curr])
-            {
-                if(x[1]!=0)
-                {
-                    queue.add(x[0]);
-                    min = Math.min(min,x[1]);
-                }
+            for(int[] x:a[curr]){    
+                queue.add(x[0]);
+                min = Math.min(min,x[1]);
             }
         }
-        return ans;
+        return min;
     }
 }
