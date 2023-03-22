bed103fa3c428ede20338353db88523bf548128d
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-03-23 00:13:51 +0530

    Update 0053. Maximum Subarray.java

--- Medium Tagged/0053. Maximum Subarray.java
+++ Medium Tagged/0053. Maximum Subarray.java
@@ -1,3 +1,4 @@
+// Kadane's Algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
         int max_Sum=Integer.MIN_VALUE;
@@ -11,5 +12,35 @@ class Solution {
                 sum=0;
         }
         return max_Sum;
+    }
+}
+
+// Divide and Conquer
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return solve(nums,0,nums.length-1);
+    }
+
+    private int solve(int[] nums,int l,int r){
+        if(l==r){
+            return nums[l];
+        }
+        int m = (l+r)/2;
+        int left = solve(nums,l,m);
+        int right = solve(nums,m+1,r);
+      
+        int sum=0, a=m, b=m+1;
+
+        int lSum=nums[a], rSum=nums[b];
+        while(a>=l){
+            sum+=nums[a--];
+            lSum = Math.max(sum,lSum);
+        }
+        sum=0;
+        while(b<=r){
+            sum+=nums[b++];
+            rSum = Math.max(sum,rSum);
+        }
+        return Math.max(lSum+rSum,Math.max(left,right));
     }
 }
