4324d648f27684c4b56e2cc7209dd52f35c1edba
Nishant <91368799+nishant7372@users.noreply.github.com>
2023-04-23 14:04:07 +0530

    Update 2652. Sum Multiples.java

--- Easy Tagged/2652. Sum Multiples.java
+++ Easy Tagged/2652. Sum Multiples.java
@@ -1,5 +1,11 @@
 class Solution {
-    public int findDelayedArrivalTime(int a, int b) {
-        return (a+b)%24;
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
     }
 }
